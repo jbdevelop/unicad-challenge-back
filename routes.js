126cadbb66651ffd2f588247483ba2e4027c3bdd
jbdevelop <jbernardorj@gmail.com>
2020-07-27 21:06:27 -0300

    remove root files

--- routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const routes = new Router()
-
-const DeliveryController = require('./src/controllers/DeliveryController')
-
-routes.post('/create', DeliveryController.store);
-routes.get('/list', DeliveryController.show);
-
-module.exports = routes
